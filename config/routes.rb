32d6e67a0539bea340224c4b9bb54cd81d5fc90e
Ocha-Misaki <himawari.158fuji@gmail.com>
2024-03-06 11:14:41 +0900

    foodsコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'application#index'
+  root 'foods#index'
+  resources :foods, only: %i[index show]
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions',
