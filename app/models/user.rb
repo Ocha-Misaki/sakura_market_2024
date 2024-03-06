1c8da6698ba687cacf1a4f266cbff4fc1f002110
Ocha-Misaki <himawari.158fuji@gmail.com>
2024-03-06 10:18:05 +0900

    userモデルにスコープを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+  scope :default_order, -> { order(id: :asc) }
 end
