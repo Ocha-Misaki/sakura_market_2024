ba93eb64ca80c12867b336dd7a0dc9d5b458d3e9
Ocha-Misaki <himawari.158fuji@gmail.com>
2024-03-07 15:10:46 +0900

    cart_helperを追加

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,17 +1,29 @@
 module CartsHelper
+  def subtotal_text
+    number_to_currency(current_cart.subtotal).to_s
+  end
+
   def total_price_including_tax_text
     number_to_currency(current_cart.total_price_including_tax).to_s
   end
 
   def purchase_button
     if current_cart&.cart_items&.empty? || !current_cart&.orderable?
-      button_tag '注文する', class: 'btn btn-outline-danger disabled'
+      button_tag 'レジに進む', class: 'btn btn-outline-danger disabled'
     else
-      button_to '注文する', '#', method: :post, class: 'btn btn-outline-danger', data: { turbo_confirm: '注文してよろしいですか？' }
+      link_to 'レジに進む', new_order_path, class: 'btn btn-outline-danger'
     end
   end
 
   def total_cart_items_quantity_text
-    "#{current_cart.cart_items.sum(&:quantity)}個"
+    "#{current_cart.total_cart_items_quantity}個"
+  end
+
+  def shipping_fee_text
+    number_to_currency(current_cart.shipping_fee).to_s
+  end
+
+  def cash_on_delivery_fee_text
+    number_to_currency(current_cart.cash_on_delivery_fee).to_s
   end
 end
