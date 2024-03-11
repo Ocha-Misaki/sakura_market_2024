2190be039b1ef096b5fbed6b117e8583983f79e6
Ocha-Misaki <himawari.158fuji@gmail.com>
2024-03-11 11:19:05 +0900

    factoryを追加

--- spec/factories/order_items.rb
+++ spec/factories/order_items.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :order_item do
-    
+    food
+    order
+    food_name { food.name }
+    food_price { food.price }
+    food_quantity { 3 }
   end
 end
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :order do
-    
+    user
+    order_name { 'テスト太郎' }
+    order_address { '東京都大田区田園調布' }
+    shipping_fee { 600 }
+    cash_on_delivery_fee { 300 }
+    delivery_time { '8:00-12:00' }
+    delivery_on { '2024-03-29' }
   end
 end
