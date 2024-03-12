baad26c2b5f2e2934420dc8ee2fc77110a9dde6c
Ocha-Misaki <himawari.158fuji@gmail.com>
2024-03-12 18:05:22 +0900

    commentテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_12_013313) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_12_073839) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_12_013313) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.text "text", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "foods", force: :cascade do |t|
@@ -155,6 +164,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_12_013313) do
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "foods"
   add_foreign_key "carts", "users"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "order_items", "foods"
