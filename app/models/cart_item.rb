09427f3f0b6ab543e885141730183738524d66c9
Ocha-Misaki <himawari.158fuji@gmail.com>
2024-03-12 16:34:35 +0900

    商品をカート追加する際に1以上を選択できないようにした

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,7 +1,7 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :food
-  validates :quantity, presence: true, numericality: { only_integer: true }
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
   scope :adding_order, -> { order(:food_id) }
 
   def price_including_tax
