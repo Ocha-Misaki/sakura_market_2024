f5d8595d8e427f4689d77fd5a7335cd12f0e09b0
Ocha-Misaki <himawari.158fuji@gmail.com>
2024-03-12 14:20:37 +0900

    likesテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_11_042706) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_12_013313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -90,6 +90,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_042706) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
+  end
+
   create_table "order_items", force: :cascade do |t|
     t.bigint "food_id", null: false
     t.bigint "order_id", null: false
@@ -146,6 +155,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_042706) do
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "foods"
   add_foreign_key "carts", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "order_items", "foods"
   add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
